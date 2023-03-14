3b6e4f7716c1d9087a920dafbb5a9543be62caae
SCBenson <sb3520@ic.ac.uk>
2023-03-14 16:04:59 +0000

    maps,encoder and decoder functions implemented

--- Main.cpp
+++ Main.cpp
@@ -18,19 +18,26 @@ std::unordered_map<int, char> vec_2_itos(std::vector<char> s) {
 	std::unordered_map<int,char> itos;
 	int count = 0;
 	for (auto i : s) {
-		itos[i] = count;
+		itos[count] = i;
 		count++;
 	}
 	return itos;
 }
 
-std::vector<int> encoder(std::unordered_map<char, int> stoi) {
+std::vector<int> encoder(std::unordered_map<char, int> stoi, std::vector<char> &text) {
 	std::vector<int> encoded_chars;
-
+	for (int i = 0; i < text.size(); i++) {
+		text[i] = stoi[text[i]];
+	}
 	return encoded_chars;
 }
-
-
+std::vector<char> decoder(std::unordered_map<char, int> itos, std::vector<int> &encoded_text) {
+	std::vector<char> decoded_chars(encoded_text.size()); //initialize the size of the vector.
+	for (int i = 0; i < encoded_text.size(); i++) {
+		decoded_chars.push_back(itos[encoded_text[i]]);
+	}
+	return decoded_chars;
+}
 
 
 int main() {
@@ -54,18 +61,53 @@ int main() {
 	//iterator
 	ip = std::unique(sorted_text.begin(), sorted_text.end());
 	sorted_text.resize(std::distance(sorted_text.begin(), ip));
-	for (auto i : sorted_text) {
-		std::cout << i;
-	}
+	std::cout << text.size();
 	/*
-	* ---------------------------------------
-	  Calling the encoding and decoding func.
-	* ---------------------------------------
+	* --------------------------------------------------------------- *
+	  Mapping char to int AND Calling the encoding and decoding func.
+	* --------------------------------------------------------------- *
 	*/
 	std::unordered_map<char, int> stoi = vec_2_stoi(sorted_text);
 	std::unordered_map<int, char> itos = vec_2_itos(sorted_text);
 
+	std::vector<int> encoded_text = encoder(stoi, text);
 
+	/*
+	* -------------------------- *
+	  Train & Test Dataset Split
+	* -------------------------- *
+	*/
+
+
+
+
+
+	/*
+	* -------------------------- *
+	  Tensor creation of dataset
+	* -------------------------- *
+	*/
+
+
+
+
+
+	/*
+	* ------------- *
+	  Loss Function
+	* ------------- *
+	*/
+
+
+
+
+
+
+	/*
+	* --------------------------- *
+	  Neural Network Architecture
+	* --------------------------- *
+	*/
 
 	return 0;
 }
