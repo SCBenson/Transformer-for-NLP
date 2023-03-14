b5b18098e8ff0863cefb21ecbbb17d0987c82a8d
SCBenson <sb3520@ic.ac.uk>
2023-03-14 10:06:17 +0000

    fixing push

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,35 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <unordered_map>
+
+std::unordered_map<char,int> vec_2_stoi(std::vector<char> s) {
+	std::unordered_map<char, int> stoi;
+	int count = 0;
+	for (auto i : s) {
+		stoi[i] = count;
+		count++;
+	}
+	return stoi;
+}
+std::unordered_map<int, char> vec_2_itos(std::vector<char> s) {
+	std::unordered_map<int,char> itos;
+	int count = 0;
+	for (auto i : s) {
+		itos[i] = count;
+		count++;
+	}
+	return itos;
+}
+
+std::vector<int> encoder(std::unordered_map<char, int> stoi) {
+	std::vector<int> encoded_chars;
+
+	return encoded_chars;
+}
+
+
+
 
 int main() {
 	
@@ -15,19 +44,28 @@ int main() {
 	while (myFile.get(c)) {
 		text.push_back(c);
 	}
-	//sort the vector in terms of character order:
+	/*
+	* ---------------------------------------
+	  Sorting the vector for character order.
+	* ---------------------------------------
+	*/
 	std::vector<char> sorted_text = text;
 	std::sort(sorted_text.begin(), sorted_text.end());
 	//iterator
 	ip = std::unique(sorted_text.begin(), sorted_text.end());
 	sorted_text.resize(std::distance(sorted_text.begin(), ip));
-	std::cout << text.size();
-	//for (ip = text.begin(); ip != text.end(); ip++) {
-		//std::cout << *ip << " ";
-	//}
-	//for (int i = 0; i < 1000; i++) {
-		//std::cout << text[i];
-	//}
-	
+	for (auto i : sorted_text) {
+		std::cout << i;
+	}
+	/*
+	* ---------------------------------------
+	  Calling the encoding and decoding func.
+	* ---------------------------------------
+	*/
+	std::unordered_map<char, int> stoi = vec_2_stoi(sorted_text);
+	std::unordered_map<int, char> itos = vec_2_itos(sorted_text);
+
+
+
 	return 0;
 }
